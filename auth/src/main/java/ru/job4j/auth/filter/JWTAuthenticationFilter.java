ba6b5aa403636e1f830ecee27f2d64931f7bfbe7
vovkalexander <vovk.ag747@gmail.com>
2023-11-03 08:00:42 +0300

    jwt fix

--- auth/src/main/java/ru/job4j/auth/filter/JWTAuthenticationFilter.java
+++ auth/src/main/java/ru/job4j/auth/filter/JWTAuthenticationFilter.java
@@ -22,7 +22,7 @@ import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
     public static final String SECRET = "SecretKeyToGenJWTs";
-    public static final long EXPIRATION_TIME = 864_000_000; /* 10 days */
+    public static final long EXPIRATION_TIME = 864_000_000;
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String SIGN_UP_URL = "/users/sign-up";
--- auth/src/main/java/ru/job4j/auth/filter/JWTAuthorizationFilter.java
+++ auth/src/main/java/ru/job4j/auth/filter/JWTAuthorizationFilter.java
@@ -2,13 +2,10 @@ package ru.job4j.auth.filter;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
-
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import org.springframework.stereotype.Component;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -43,7 +40,6 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
     private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest request) {
         String token = request.getHeader(HEADER_STRING);
         if (token != null) {
-            /* parse the token. */
             String user = JWT.require(Algorithm.HMAC512(SECRET.getBytes()))
                     .build()
                     .verify(token.replace(TOKEN_PREFIX, ""))
